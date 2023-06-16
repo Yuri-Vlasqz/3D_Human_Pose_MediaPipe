0e388681d14dca4d53ca36390c9ae1191f8174b0
Yuri Velasquez <106136458+Yuri-Vlasqz@users.noreply.github.com>
2023-06-16 01:10:40 -0300

    Update Pose3dModule.py

--- Pose3dModule.py
+++ Pose3dModule.py
@@ -5,18 +5,14 @@ from itertools import combinations
 import matplotlib.pyplot as plt
 
 
-# from mpl_toolkits.mplot3d import Axes3D
-# from mpl_toolkits.mplot3d.art3d import Line3DCollection
-
-
 class MpPose2D:
     def __init__(self):
-        # MediaPipe 2D Pose Model
+        # MediaPipe 2D Pose Model (https://developers.google.com/mediapipe/solutions/vision/pose_landmarker/index#models)
         self.mpDraw = mp.solutions.drawing_utils
         self.mpDrawingStyles = mp.solutions.drawing_styles
         self.mpPose = mp.solutions.pose
         self.pose = self.mpPose.Pose(
-            model_complexity=1,
+            model_complexity=1,  # 1 = Pose landmarker (Full)
             min_detection_confidence=0.75,
             min_tracking_confidence=0.75
         )
@@ -29,8 +25,7 @@ class MpPose2D:
                 'pixel_xy': [norm_point.x * image_width, norm_point.y * image_height],
                 'visibility': norm_point.visibility
             })
-        # min_vis_landmark = min(landmarks_list, key=lambda x: x['visibility'])
-        # print(min_vis_landmark['visibility'])
+        
         return landmarks_list
 
     def calc_2d_kpt(self, image, draw=True):
@@ -50,7 +45,7 @@ class MpPose2D:
         if results.pose_landmarks:
             # Pose Landmarks list in sub-pixels coordinates
             landmarks_list = self.mp_landmarks_to_list(results.pose_landmarks, image_width=w, image_height=h)
-            # print(f"\n-{len(landmarks_list)} landmarks: {landmarks_list}")
+            
             if draw:
                 # Draw the pose landmarks on the image.
                 self.mpDraw.draw_landmarks(
@@ -107,20 +102,17 @@ class Pose3D:
         # Homgeneous points are returned
         euclid_points_3d = cv2.convertPointsFromHomogeneous(np.transpose(homog_points_3d))
 
-        # Manual euclid calculation
-        # points_3d /= points_3d[3]
-        # return points_3d[:-1]
-
         return euclid_points_3d
 
-    def calc_avg_3d_kpt(self, all_2d_poses):
-        # all_3d_poses = np.empty([len(), len()], dtype=np.ndarray)
+    def calc_avg_3d_kpt(self, all_2d_poses):  # Not recomended, debugging required !!!
+        
         all_3d_poses = []
         all_min_vis = []
+        
         for i, j in self.pairs:
             # triangulate 3d points per camera pair
             pose_3d_pair = self.cv2_3d_pose_pair(pair=[i, j], poses=all_2d_poses)
-            # list o minimum visibility of the pair in each joint
+            # List o minimum visibility of the pair in each joint
             min_visibility_pair = [min(joint1['visibility'], joint2['visibility'])
                                    for joint1, joint2 in zip(all_2d_poses[i], all_2d_poses[i])]
             all_min_vis.append(min_visibility_pair)
@@ -163,7 +155,7 @@ class Pose3D:
 
         # Finding array/pose index with best visisibility in all all_min_vis
         best_pair_idx = np.array(all_min_vis).argmax(axis=0)
-        # best 3d pose
+        # 3d pose with *best* visibility for each joint
         best_3d_pose = [all_3d_poses[pair_idx][joint_idx, 0] for joint_idx, pair_idx in enumerate(best_pair_idx)]
 
         return np.array(best_3d_pose)
@@ -179,26 +171,16 @@ class Pose3D:
         ax.set_xlabel('Z')
         ax.set_ylabel('Y')
         ax.set_zlabel('X')
-
-        # set axes ticks (single person in meters)
-        # ax.set_xticks(range(-2, 3))
-        # ax.set_yticks(range(-2, 3))
-        # ax.set_zticks(range(0, 5))
-        # # Set limits for the x, y, and z axes
-        # ax.set_xlim((-2, 2))
-        # ax.set_ylim((-2, 2))
-        # ax.set_zlim((0, 4))
-
+        
         # set axes ticks (Panoptic in centimeters)
         # Set limits for the x, y, and z axes
         ax.set_xlim((-200, 200))
         ax.set_zlim((-200, 200))
         ax.set_ylim((000, -400))
 
-        # set cameras positions
+        # Draw cameras positions
         for i, camera in enumerate(camera_positions):
             # Add the points to the plot
-            # ax.scatter(xs=camera[0, 0], ys=camera[0, 1], zs=camera[0, 2])  # single person
             # panoptic
             cc = (-camera.transpose() * camera_orientations[i])
             ax.scatter(xs=cc[0, 0], ys=cc[0, 1], zs=cc[0, 2], marker='.', linewidths=6)
@@ -207,15 +189,7 @@ class Pose3D:
 
     @staticmethod
     def update_3d_pose(pose_3d, axes):
-        # camera_positions = self.T_vecs
-        #
-        # # Create a new 3D plot
-        # fig = plt.figure()
-        # ax = fig.add_subplot(111, projection='3d')
-        #
-        # for camera in camera_positions:
-        #     # Add the points to the plot
-        #     ax.scatter(xs=camera[0, 0], ys=camera[0, 1], zs=camera[0, 2])
+        
         for artist in axes.lines:
             artist.remove()
 
@@ -230,61 +204,4 @@ class Pose3D:
 
         plt.pause(0.0001)
 
-        # # Set labels for the axes
-        # ax.set_xlabel('Z')
-        # ax.set_ylabel('X')
-        # ax.set_zlabel('Y')
-        # # set axes ticks
-        # ax.set_xticks(range(-2, 3))
-        # ax.set_yticks(range(-2, 3))
-        # ax.set_zticks(range(0, 5))
-        # # Set limits for the x, y, and z axes
-        # ax.set_xlim((-2, 2))
-        # ax.set_ylim((-2, 2))
-        # ax.set_zlim((0, 4))
-        # Show the plot
-
-        # for connection in connections_frozenset:
-        #     print(i)
-        #     Axes3D.plot(xs, ys, *args, zdir='z', **kwargs)
-        #     ax.plot(xs=[kpts3d[_c[0], 1], kpts3d[_c[1], 1]],
-        #             ys=[kpts3d[_c[0], 0], kpts3d[_c[1], 0]],
-        #             zs=[kpts3d[_c[0], 2], kpts3d[_c[1], 2]],
-        #             linewidth=4, c=part_color)
-
-
-""" MediaPipe 33 pose landmarks
-NOSE = 0
-LEFT_EYE_INNER = 1
-LEFT_EYE = 2
-LEFT_EYE_OUTER = 3
-RIGHT_EYE_INNER = 4
-RIGHT_EYE = 5
-RIGHT_EYE_OUTER = 6
-LEFT_EAR = 7
-RIGHT_EAR = 8
-MOUTH_LEFT = 9
-MOUTH_RIGHT = 10
-LEFT_SHOULDER = 11
-RIGHT_SHOULDER = 12
-LEFT_ELBOW = 13
-RIGHT_ELBOW = 14
-LEFT_WRIST = 15
-RIGHT_WRIST = 16
-LEFT_PINKY = 17
-RIGHT_PINKY = 18
-LEFT_INDEX = 19
-RIGHT_INDEX = 20
-LEFT_THUMB = 21
-RIGHT_THUMB = 22
-LEFT_HIP = 23
-RIGHT_HIP = 24
-LEFT_KNEE = 25
-RIGHT_KNEE = 26
-LEFT_ANKLE = 27
-RIGHT_ANKLE = 28
-LEFT_HEEL = 29
-RIGHT_HEEL = 30
-LEFT_FOOT_INDEX = 31
-RIGHT_FOOT_INDEX = 32
-"""
+        
